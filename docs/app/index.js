9ab24703ee8ca99fbcff25cc094762ed5ff14ce3
Jordan McArdle <jordanmcardle@gmail.com>
2016-11-01 04:44:08 -0500

    Docs portion working - need to generate build folder for github pages

--- docs/app/index.js
+++ docs/app/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Router, Route, useRouterHistory } from 'react-router';
+import { Router, Route } from 'react-router';
 import createHistory from 'history/lib/createBrowserHistory';
-// import createHashHistory from 'history/createHashHistory';
 
 import Home from './components/layout/home';
 import Install from './components/layout/install';
 import Main from './components/layout/main';
 
-// const history = useRouterHistory(createHashHistory)(
 const appHistory = createHistory(
   {
-    // basename: __dirname,
     hashType: 'slash'
-    // queryKey: false
   }
 );
-// const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 appHistory.push('/');
-
 
 ReactDOM.render((
   <Router history={appHistory}>
